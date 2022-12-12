a805a662dab7d5a80b02c6767857c6180b8fb88b
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2022-12-12 17:16:13 +0400

    feat: Add data and run map on it

--- src/controllers/listingcontroller.js
+++ src/controllers/listingcontroller.js
@@ -2,5 +2,22 @@ import Listing from "../models/listing.js";
 
 export const getAllListings = async (req, res) => {
   const data = await Listing.find();
-  return res.json(data);
+  const newData = data.map((listings) => {
+    return {
+      id: listings.id,
+      company: listings.company,
+      logo: listings.logo,
+      new: listings.new,
+      featured: listings.featured,
+      position: listings.position,
+      role: listings.role,
+      level: listings.level,
+      postedAt: listings.postedAt,
+      contract: listings.contract,
+      location: listings.location,
+      languages: listings.languages,
+      tools: listings.tools,
+    };
+  });
+  return res.json(newData);
 };
